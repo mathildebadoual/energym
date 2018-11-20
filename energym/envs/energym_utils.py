3144ddbfcb2f8f61a5924ec15ba4d5bad823b4ca
Mathilde Badoual <mathilde.badoual@gmail.com>
2018-11-20 15:17:11 -0800

    delete utils

--- energym/envs/energym_utils.py
+++
@@ -1,9 +0,0 @@
-
-class EmptyDataException(Exception):
-    def __init__(self):
-        super().__init__()
-
-
-class OptimizationException(Exception):
-    def __init__(self):
-        super().__init__()
