66237cdc9811c2a02c636c780627598e80d05d4c
Mathilde Badoual <mathilde.badoual@gmail.com>
2019-03-26 15:54:30 -0700

    create 2 subfolders

---
+++ energym/envs/grid_scale/battery_env.py
@@ -0,0 +1,95 @@
+import gym
+import numpy as np
+from gym import error, spaces, utils
+from gym.utils import seeding
+
+import logging
+logger = logging.getLogger(__name__)
+
+
+class BatteryEnv(gym.Env):
+    metadata = {'render.modes': ['human']}
+
+    def __init__(self):
+        # limitation parameters
+        self._max_soe = 3000  # soe = state of energy in MWh
+        self._min_soe = 0
+        self._max_power = 1000  # power to charge or discharge in MW
+        self._min_power = - 1000
+        self._efficiency_ratio = 0.99
+
+        # cost when close to limits
+        self._cost_lim_power = 10
+        self._cost_lim_soe = 10
+
+        # gym variables
+        self.observation_space = spaces.Box(low=self._min_soe, high=self._max_soe, shape=(1,), dtype=np.float32)
+        # TODO(Mathilde): Add option for a discrete action space (for now it is continuous)
+        self.action_space = spaces.Box(low=self._min_power, high=self._max_power, shape=(1,), dtype=np.float32)
+
+        self._state = self.reset()
+
+    def step(self, action):
+        if not isinstance(action, np.ndarray):
+            action = np.array([action])
+
+        reward = self.get_penalty(action)
+
+        energy_to_add = - self._efficiency_ratio * action
+        next_soe = self._state + energy_to_add
+
+        if self.action_space.contains(abs(energy_to_add)) and self.observation_space.contains(next_soe):
+            self._state = next_soe
+
+        ob = self._get_obs()
+
+        # TODO(Mathilde): define when it is done for this env
+        done = False
+        return ob, reward, done, dict()
+
+    def reset(self):
+        # TODO(Mathilde): look at the gym.spaces.Box doc to see if we can randomly select a element in the "box"
+        self._state = self.observation_space.sample()
+        # self._state = np.array([0])
+        return self._get_obs()
+
+    def render(self, mode='rgb_array'):
+        print('current state:', self._state)
+
+    def _get_obs(self):
+        # to assure we are not overwriting on the state
+        return np.copy(self._state)
+
+    def seed(self, seed=None):
+        if seed is not None:
+            np.random.seed = seed
+        else:
+            np.random.seed = seeding.np_random()
+
+    def get_parameters_battery(self):
+        return self._max_soe, self._min_soe, self._max_power, self._min_power, self._efficiency_ratio
+
+    def get_penalty(self, power):
+        if not isinstance(power, np.ndarray):
+            power = np.array([power])
+        energy_to_add = self._efficiency_ratio * power
+        # print('energy_to_add: ', energy_to_add)
+        next_soe = self._state - energy_to_add
+        # print('next_soe: ', next_soe)
+        reward = 0
+        cost_lim_power = self._cost_lim_power * (min(self._max_power - energy_to_add[0], 0) + min(energy_to_add[0] - self._min_power, 0))
+        cost_lim_soe = self._cost_lim_soe * (min(self._max_soe - next_soe[0], 0) + min(next_soe[0] - self._min_soe, 0))
+        # if self.action_space.contains(abs(energy_to_add)) and self.observation_space.contains(next_soe):
+        #     reward = -1000
+        reward = cost_lim_power + cost_lim_soe
+        return reward
+
+    def is_safe(self, power):
+        if not isinstance(power, np.ndarray):
+            power = np.array([power])
+        energy_to_add = - self._efficiency_ratio * power
+        next_soe = self._state + energy_to_add
+        if self.action_space.contains(abs(energy_to_add)) and self.observation_space.contains(next_soe):
+            return True
+        else:
+            return False
---
+++ energym/envs/grid_scale/energy_market_battery_env_v0.py
@@ -0,0 +1,115 @@
+import gym
+import numpy as np
+import datetime
+from gym import error, spaces, utils
+from gym.utils import seeding
+from energym.envs.utils import OptimizationException, EmptyDataException
+
+import logging
+logger = logging.getLogger(__name__)
+
+# TODO(Mathilde): Explain well what is this environment...
+
+
+class EnergyMarketBatteryEnv(gym.Env):
+    def __init__(self):
+        # TODO(Mathilde): If different modes, should take only continous envs + delta_time and start_date not defined here ...
+        self._energy_market = gym.make('energy_market-v0')
+        self._battery = gym.make('battery-v0')
+        self._state = np.array([0, 0, 0, 0, 0], dtype=np.float32)
+        self._n_discrete_cost = 200
+        self._n_discrete_power = 200
+        self._n_discrete_actions = self._n_discrete_power * self._n_discrete_cost
+        self._min_cost, self._max_cost = 0, 100
+        self._min_power, self._max_power = self._battery.action_space.low[0], self._battery.action_space.high[0]
+
+        # gym variables
+        self.observation_space = spaces.Box(low=-np.inf, high=np.inf,
+                                            shape=(self._battery.observation_space.shape[0] +
+                                                   self._energy_market.observation_space.shape[0],),
+                                            dtype=np.float32)
+        self.action_space = spaces.Discrete(self._n_discrete_actions)
+
+    def step(self, action):
+        if isinstance(action, int) or isinstance(action, np.int64):
+            action = int(action)
+            power, cost = self.discrete_to_continuous_action(action)
+        else:
+            power, cost = action[0], action[1]
+            
+        done = False
+
+        # first put the penalty (shielding)
+        reward = self._battery.get_penalty(np.array([power]))
+
+        try:
+            ob_market, _, done, info_market = self._energy_market.step(np.array([power, cost]))
+        except OptimizationException:
+            self._state = np.zeros(self.observation_space.shape[0])
+            ob = self._get_obs()
+            print('optimization exception')
+            return ob, reward, done, dict()
+
+        power_cleared = ob_market[0]
+
+        date = self._energy_market._date
+
+        if -2 < power_cleared < 2 and power != 0:
+            power_cleared = 0
+            reward += -50
+
+        ob_battery, reward_battery, _, _ = self._battery.step(power_cleared)
+
+        # define state and reward
+        self._state = np.concatenate((ob_market, ob_battery))
+        if reward_battery == 0 and not done:
+            reward += min(power_cleared, 0) * info_market['price_cleared']
+            reward += max(power_cleared, 0) * cost
+        ob = self._get_obs()
+
+        return ob, reward, done, dict({'date': date, 'price_cleared': info_market['price_cleared'], 'ref_price': info_market['ref_price'], 'price_bid': cost})
+
+    def reset(self, start_date=None):
+        ob_market = self._energy_market.reset(start_date=start_date)
+        ob_battery = self._battery.reset()
+        self._state = np.concatenate((ob_market, ob_battery))
+        return self._get_obs()
+
+    def set_test(self):
+        self._energy_market.set_test()
+
+    def render(self, mode='rgb_array'):
+        print('Current state:', self._state)
+
+    def _get_obs(self):
+        # to assure we are not overwriting on the state
+        return np.copy(self._state)
+
+    def seed(self, seed=None):
+        if seed is not None:
+            np.random.seed = seed
+        else:
+            np.random.seed = seeding.np_random()
+
+    @property
+    def _power_precision(self):
+        return (self._max_power - self._min_power) / self._n_discrete_power
+
+    @property
+    def _cost_precision(self):
+        return (self._max_cost - self._min_cost) / self._n_discrete_cost
+
+    def discrete_to_continuous_action(self, discrete_action):
+        """
+        maps the integer discrete_action to the grid (power, cost)
+        :param discrete_action: int
+        :return: (float, float)
+        """
+        power = self._min_power + (discrete_action % self._n_discrete_power) * self._power_precision
+        cost = self._min_cost + (discrete_action // self._n_discrete_power) * self._cost_precision
+
+        return power, cost
+
+    def is_safe(self, action_index):
+        power, cost = self.discrete_to_continuous_action(action_index)
+        return self._battery.is_safe(power)
